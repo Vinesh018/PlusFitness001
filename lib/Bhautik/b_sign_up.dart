1e9d453debc829acb25a61f6125d1c5a8d5c939f
Bhautik Patel <thummarbhautik7045@gmail.com>
2024-03-13 10:14:29 +0530

    changed kcal

---
+++ lib/Bhautik/b_sign_up.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainSignUpPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
