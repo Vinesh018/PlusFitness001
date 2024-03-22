6d5b7a46cc4ea19018daf183e436742d99715606
Vinesh Thakkar <vinesh.thakkar@plusinfosys.com>
2024-03-22 16:12:51 +0530

    temp

--- lib/Bhautik/Myprofilesubpages/personaldata.dart
+++ lib/Bhautik/Myprofilesubpages/personaldata.dart
@@ -22,7 +22,9 @@ class PersonalDataMainShow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 231, 235, 237),
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 231, 235, 237),
         leading: IconButton(
           onPressed: () {
             Get.off(UserProfileMainRun());
